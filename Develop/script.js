97e5ddabd475d0d3ece7bae7230cd3663c157e08
alexo-a <a.ostrowski.11@gmail.com>
2020-06-14 12:05:51 -0500

    added html options for character types

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,9 @@
 // Assignment code here
+var selectedCharacterTypes = [];
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -15,3 +16,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add event listener to chooseOptions button
+chooseOptionsBtn.addEventListener("click", showOptions);
